98375e200c628cdaa3997aa6916b4cbbd3ea2598
Satyam Kumar <ksatyam433@gmail.com>
2023-08-20 16:42:15 +0530

    Added routes and favicon

--- leafy-lodgings/src/App.jsx
+++ leafy-lodgings/src/App.jsx
@@ -1,41 +1,67 @@
-import styled from "styled-components";
-
-const H1 = styled.h1`
-font-size:30px;
-font-weight: 600;
-`
-const Button = styled.button`
-  font-size:1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: violet;
-  color: white;
-  cursor: pointer;
-  margin: 20px;
-`
-
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2 rem;
-`
-
-const StyledApp = styled.main`
-  background-color: orange;
-  padding: 20px;
-`
-
-
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import GLobalStyles from "./styles/GlobalStyles";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
 function App() {
-    return <StyledApp>
-      <H1>Leafy Lodgings</H1>
-      <Button> Check In </Button>
-      <Button> Check In </Button>
-
-      <Input type='number' placeholder='Number of Guests' />
-    </StyledApp>;
+    return (
+        <>
+            <GLobalStyles />
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<AppLayout />}>
+                        <Route
+                            index
+                            element={
+                                <Navigate
+                                    replace
+                                    to='dashboard'
+                                />
+                            }
+                        />
+                        <Route
+                            path='dashboard'
+                            element={<Dashboard />}
+                        />
+                        <Route
+                            path='bookings'
+                            element={<Bookings />}
+                        />
+                        <Route
+                            path='cabins'
+                            element={<Cabins />}
+                        />
+                        <Route
+                            path='users'
+                            element={<Users />}
+                        />
+                        <Route
+                            path='settings'
+                            element={<Settings />}
+                        />
+                        <Route
+                            path='account'
+                            element={<Account />}
+                        />
+                    </Route>
+                    <Route
+                        path='login'
+                        element={<Login />}
+                    />
+                    <Route
+                        path='*'
+                        element={<PageNotFound />}
+                    />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
--- leafy-lodgings/src/ui/Logo.jsx
+++ leafy-lodgings/src/ui/Logo.jsx
@@ -1,20 +1,23 @@
 import styled from "styled-components";
 
 const StyledLogo = styled.div`
-  text-align: center;
+    text-align: center;
 `;
 
 const Img = styled.img`
-  height: 9.6rem;
-  width: auto;
+    height: 6rem;
+    width: auto;
 `;
 
 function Logo() {
-  return (
-    <StyledLogo>
-      <Img src="/logo-light.png" alt="Logo" />
-    </StyledLogo>
-  );
+    return (
+        <StyledLogo>
+            <Img
+                src='/logo.png'
+                alt='Logo'
+            />
+        </StyledLogo>
+    );
 }
 
 export default Logo;
