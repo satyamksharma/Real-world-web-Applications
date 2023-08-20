55dba1be98066cd40f26f0fc63231339a7131e78
Satyam Kumar <ksatyam433@gmail.com>
2023-08-20 15:20:19 +0530

    Added styled components

--- leafy-lodgings/src/App.jsx
+++ leafy-lodgings/src/App.jsx
@@ -1,5 +1,41 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+font-size:30px;
+font-weight: 600;
+`
+const Button = styled.button`
+  font-size:1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: violet;
+  color: white;
+  cursor: pointer;
+  margin: 20px;
+`
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2 rem;
+`
+
+const StyledApp = styled.main`
+  background-color: orange;
+  padding: 20px;
+`
+
+
 function App() {
-    return <div>Hello</div>;
+    return <StyledApp>
+      <H1>Leafy Lodgings</H1>
+      <Button> Check In </Button>
+      <Button> Check In </Button>
+
+      <Input type='number' placeholder='Number of Guests' />
+    </StyledApp>;
 }
 
 export default App;
