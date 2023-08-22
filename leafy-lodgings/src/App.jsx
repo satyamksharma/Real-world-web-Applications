e35d17f6dba774506015a21c53c98f65e1396a0e
Satyam Kumar <ksatyam433@gmail.com>
2023-08-22 16:57:09 +0530

    Added react query

--- leafy-lodgings/src/App.jsx
+++ leafy-lodgings/src/App.jsx
@@ -9,9 +9,19 @@ import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 1000 * 60,
+        },
+    },
+});
+
 function App() {
     return (
-        <>
+        <QueryClientProvider client={queryClient}>
             <GLobalStyles />
             <BrowserRouter>
                 <Routes>
@@ -60,7 +70,7 @@ function App() {
                     />
                 </Routes>
             </BrowserRouter>
-        </>
+        </QueryClientProvider>
     );
 }
 
