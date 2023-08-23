9345cc8b380a84321c9514e35658dfcf4a1b6941
Satyam Kumar <ksatyam433@gmail.com>
2023-08-24 00:27:27 +0530

    added form and file upload

--- leafy-lodgings/src/services/apiCabins.js
+++ leafy-lodgings/src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
     const { data, error } = await supabase.from("cabins").select("*");
@@ -10,11 +10,32 @@ export async function getCabins() {
 }
 
 export async function createCabin(newCabin) {
-    const { data, error } = await supabase.from("cabins").insert([newCabin]);
+    const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll("/", "");
+    const imagePath = `${supabaseUrl}/storage/v1/object/sign/cabin-images/${imageName}`;
+
+    // https://oxjsltpyeodateohtyia.supabase.co/storage/v1/object/sign/cabin-images/cabin-001.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJjYWJpbi1pbWFnZXMvY2FiaW4tMDAxLmpwZyIsImlhdCI6MTY5MjU2MTg1NSwiZXhwIjoxNzI0MDk3ODU1fQ.9cTNdCWuOP0XPtvqmf1hKeO00gpWDsi7wLaHd5GHrB0&t=2023-08-20T20%3A04%3A14.693Z
+
+    //1.Create cabin
+    const { data, error } = await supabase
+        .from("cabins")
+        .insert([{ ...newCabin, image: imagePath }]);
     if (error) {
         console.error(error);
         throw new Error("Cabin could not be created");
     }
+
+    //2. upload image
+    const { error: storageError } = await supabase.storage
+        .from("cabin-images")
+        .upload(imageName, newCabin.image);
+
+    //3.Delete the cabin if there was an error uploading the image
+    if (storageError) {
+        await supabase.from("cabins").delete().eq("id", data.id);
+        console.error(storageError);
+        throw new Error("Cabin image could not be uploaded and the cabin was not created");
+    }
+
     return data;
 }
 
