b41f34bc582d96af9050af12dd5c45400daf2b72
Satyam Kumar <ksatyam433@gmail.com>
2023-08-25 23:32:35 +0530

    Added modal for form

--- leafy-lodgings/src/features/cabins/AddCabin.jsx
+++ leafy-lodgings/src/features/cabins/AddCabin.jsx
@@ -1,18 +1,42 @@
-import Button from 'ui/Button';
-import Modal from 'ui/Modal';
-import CreateCabinForm from './CreateCabinForm';
+import { useState } from "react";
+import Button from "../../ui/Button";
+import Modal from "../../ui/Modal";
+import CreateCabinForm from "./CreateCabinForm";
+import CabinTable from "./CabinTable";
 
 function AddCabin() {
-  return (
-    <Modal>
-      <Modal.Toggle opens='new-cabin'>
-        <Button>Add new cabin</Button>
-      </Modal.Toggle>
-      <Modal.Window name='new-cabin'>
-        <CreateCabinForm />
-      </Modal.Window>
-    </Modal>
-  );
+    return (
+        <Modal>
+            <Modal.Open opens='cabin-form'>
+                <Button>Add new cabin</Button>
+            </Modal.Open>
+            <Modal.Window name='cabin-form'>
+                <CreateCabinForm />
+            </Modal.Window>
+
+            <Modal.Open opens='table'>
+                <Button>Show Table</Button>
+            </Modal.Open>
+            <Modal.Window name='table'>
+                <CabinTable />
+            </Modal.Window>
+        </Modal>
+    );
 }
+
+// function AddCabin() {
+//     const [isOpenModal, setIsOpenModal] = useState(false);
+
+//     return (
+//         <div>
+//             <Button onClick={() => setIsOpenModal((show) => !show)}>Add new cabin</Button>
+//             {isOpenModal && (
+//                 <Modal onClose={() => setIsOpenModal(false)}>
+//                     <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
+//                 </Modal>
+//             )}
+//         </div>
+//     );
+// }
 
 export default AddCabin;
