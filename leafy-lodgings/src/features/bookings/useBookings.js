39aaf9062824b4aad16b2b4fa0a8f24399149b10
Satyam Kumar <ksatyam433@gmail.com>
2023-08-30 23:44:26 +0530

    added sorting feature in bookings

--- leafy-lodgings/src/features/bookings/useBookings.js
+++ leafy-lodgings/src/features/bookings/useBookings.js
@@ -9,13 +9,19 @@ export function useBookings() {
     const filterValue = searchParams.get("status");
     const filter =
         !filterValue || filterValue === "all" ? null : { field: "status", value: filterValue };
+
+    //Sort
+    const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+    const [field, direction] = sortByRaw.split("-");
+    const sortBy = { field, direction };
+
     const {
         isLoading,
         data: bookings,
         error,
     } = useQuery({
-        queryKey: ["bookings", filter],
-        queryFn: () => getBookings({ filter }),
+        queryKey: ["bookings", filter, sortBy],
+        queryFn: () => getBookings({ filter, sortBy }),
     });
 
     return { bookings, isLoading, error };
--- leafy-lodgings/src/services/apiBookings.js
+++ leafy-lodgings/src/services/apiBookings.js
@@ -9,7 +9,10 @@ export async function getBookings({ filter, sortBy }) {
         );
 
     //Filter
-    if (filter !== null) query = query[filter.method || "eq"](filter.field, filter.value);
+    if (filter) query = query[filter.method || "eq"](filter.field, filter.value);
+
+    //Sorting
+    if (sortBy) query = query.order(sortBy.field, { ascending: sortBy.direction === "asc" });
 
     const { data, error } = await query;
 
