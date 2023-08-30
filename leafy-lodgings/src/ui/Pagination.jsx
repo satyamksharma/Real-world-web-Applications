abf84374078a1036f56610c705d0ea424ef7f3bf
Satyam Kumar <ksatyam433@gmail.com>
2023-08-31 00:35:06 +0530

    pagination done

--- leafy-lodgings/src/ui/Pagination.jsx
+++ leafy-lodgings/src/ui/Pagination.jsx
@@ -1,4 +1,5 @@
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
+import { useSearchParams } from "react-router-dom";
 import styled from "styled-components";
 
 const StyledPagination = styled.div`
@@ -57,19 +58,46 @@ const PaginationButton = styled.button`
     }
 `;
 
+const PAGE_SIZE = 10;
+
 function Pagination({ count }) {
-    function nextPage() {}
-    function prevPage() {}
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const currentPage = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
+
+    const pageCount = Math.ceil(count / PAGE_SIZE);
+
+    function nextPage() {
+        const next = currentPage === pageCount ? currentPage : currentPage + 1;
+
+        searchParams.set("page", next);
+        setSearchParams(searchParams);
+    }
+    function prevPage() {
+        const prev = currentPage === 1 ? currentPage : currentPage - 1;
+
+        searchParams.set("page", prev);
+        setSearchParams(searchParams);
+    }
+    if (pageCount <= 1) return null;
     return (
         <StyledPagination>
             <p>
-                Showing <span>1</span> to <span>10</span> of <span>{count}</span> results
+                Showing <span>{(currentPage - 1) * PAGE_SIZE + 1}</span> to{" "}
+                <span>{currentPage === pageCount ? count : currentPage * PAGE_SIZE}</span> of{" "}
+                <span>{count}</span> results
             </p>
             <Buttons>
-                <PaginationButton onClick={prevPage}>
+                <PaginationButton
+                    onClick={prevPage}
+                    disabled={currentPage === 1}
+                >
                     <HiChevronLeft /> <span>Prev</span>
                 </PaginationButton>
-                <PaginationButton onClick={nextPage}>
+                <PaginationButton
+                    onClick={nextPage}
+                    disabled={currentPage === pageCount}
+                >
                     <span>Next</span> <HiChevronRight />
                 </PaginationButton>
             </Buttons>
