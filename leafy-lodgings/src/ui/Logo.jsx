24a21ec11e617c950c7882ab4725fc9eb7feeacb
Satyam Kumar <ksatyam433@gmail.com>
2023-08-31 19:23:25 +0530

    fixed a bug

--- leafy-lodgings/src/ui/Logo.jsx
+++ leafy-lodgings/src/ui/Logo.jsx
@@ -2,24 +2,27 @@ import styled from "styled-components";
 import { useDarkMode } from "../context/DarkModeContext";
 
 const StyledLogo = styled.div`
-  text-align: center;
+    text-align: center;
 `;
 
 const Img = styled.img`
-  height: 9.6rem;
-  width: auto;
+    height: 7.6rem;
+    width: auto;
 `;
 
 function Logo() {
-  const { isDarkMode } = useDarkMode();
+    const { isDarkMode } = useDarkMode();
 
-  const src = isDarkMode ? "/logo-dark.png" : "/logo-light.png";
+    const src = isDarkMode ? "/logo-dark.png" : "/logo-light.png";
 
-  return (
-    <StyledLogo>
-      <Img src={src} alt="Logo" />
-    </StyledLogo>
-  );
+    return (
+        <StyledLogo>
+            <Img
+                src={src}
+                alt='Logo'
+            />
+        </StyledLogo>
+    );
 }
 
 export default Logo;
