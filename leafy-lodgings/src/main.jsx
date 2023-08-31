23ddcac3161f2cf139be27e5afef0fc63cdd186a
Satyam Kumar <ksatyam433@gmail.com>
2023-08-31 19:37:13 +0530

    files updated 2.0

--- leafy-lodgings/src/main.jsx
+++ leafy-lodgings/src/main.jsx
@@ -5,12 +5,12 @@ import { ErrorBoundary } from "react-error-boundary";
 import ErrorFallback from "./ui/ErrorFallback";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <ErrorBoundary
-            FallbackComponent={ErrorFallback}
-            onReset={() => window.location.replace("/")}
-        >
-            <App />
-        </ErrorBoundary>
-    </React.StrictMode>
+  <React.StrictMode>
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => window.location.replace("/")}
+    >
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
 );
